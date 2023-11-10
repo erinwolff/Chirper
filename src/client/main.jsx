216136fdd23084058fad520c6d18f819f81f59db
Erin Wolff <erin_wolff@outlook.com>
2023-11-10 11:46:37 -0800

    updated path to Posts component, removed template path

--- src/client/main.jsx
+++ src/client/main.jsx
@@ -7,18 +7,19 @@ import { Provider } from "react-redux";
 import store from "./store";
 
 import AuthForm from "./features/auth/AuthForm";
-import Tasks from "./features/tasks/Tasks";
 import Root from "./layout/Root.jsx";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Posts from "./features/posts/Posts.jsx";
+
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     children: [
-      { path: "/", element: <Tasks /> },
-      { path: "/tasks", element: <Tasks /> },
       { path: "/login", element: <AuthForm /> },
+      { path: "/posts", element: <Posts /> }
     ],
   },
 ]);
