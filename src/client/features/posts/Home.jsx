92e941f96e0508c30ec1f1af664a2a168ec7338e
Erin Wolff <erin_wolff@outlook.com>
2023-11-10 15:05:25 -0800

    completed Home component to show posts by ALL users

--- src/client/features/posts/Home.jsx
+++ src/client/features/posts/Home.jsx
@@ -1,7 +1,39 @@
-function Home(){
-  return(
-    <>Home page!</>
+import { useGetAllPostsQuery } from "./postSlice";
+
+export const HomeCard = ({ post }) => {
+  return (
+    <>
+      <ul className="post-card">
+        <section className="post-info">
+          <li className="post">
+            <p className="post-text">
+              {post.post}
+              <br />
+            </p>
+
+          </li>
+        </section>
+
+      </ul>
+    </>
   )
 }
 
-export default Home;
+export default function Home() {
+  const { data: posts, isLoading } = useGetAllPostsQuery();
+
+  return (
+    <div className="posts">
+      <h2 className="welcome">what the birds are chirping about: </h2>
+      {isLoading && <p>Loading posts...</p>}
+      {posts && (
+        <ul>
+          {posts.map((post) => (
+            <HomeCard key={post.id} post={post} />
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
